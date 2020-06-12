042efd6f761f86dc6a9276a05605dbd929e3093f
a782327 <cesar.chavezmaldonado@atos.net>
2020-06-13 00:07:40 +0200

    Update.

--- test.ps1
+++ test.ps1
@@ -1,31 +1,5 @@
-Start-Process powershell -Verb runAs
+set-executionpolicy unrestricted
 
-Write-Host –NoNewLine “Counting from 1 to 9 (in seconds):  “
-
-foreach($element in 1..3){
-
-  Write-Host –NoNewLine  “${element} ” -BackgroundColor “Green” -ForegroundColor “Black”
-
-  Start-Sleep –Seconds 1
-
-}
-
-foreach($element in 4..6){
-
-  Write-Host –NoNewLine  “${element} ” -BackgroundColor “Yellow” -ForegroundColor “Black”
-
-  Start-Sleep –Seconds 1
-
-}
-
-foreach($element in 7..9){
-
-  Write-Host –NoNewLine  “${element} ” -BackgroundColor “Red” -ForegroundColor “Black”
-
-  Start-Sleep –Seconds 1
-
-}
-
-Write-Host “”
-
-Start-Process PowerShell {[void][System.Console]::ReadKey($true)} -Wait -NoNewWindow
+Write-Host "Press any key to continue"
+#Start-Process PowerShell {[void][System.Console]::ReadKey($true)} -Wait -NoNewWindow
+[Environment]::Exit(0)
